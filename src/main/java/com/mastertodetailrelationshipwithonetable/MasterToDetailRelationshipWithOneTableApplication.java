bd5c60a9f1f0cce2e836be69db0491a2bebf6e2c
asghardahri <gasghar134@gmail.com>
2023-03-30 13:30:54 +0500

    V1

---
+++ src/main/java/com/mastertodetailrelationshipwithonetable/MasterToDetailRelationshipWithOneTableApplication.java
@@ -0,0 +1,13 @@
+package com.mastertodetailrelationshipwithonetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterToDetailRelationshipWithOneTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterToDetailRelationshipWithOneTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mastertodetailrelationshipwithonetable/MasterToDetailRelationshipWithOneTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastertodetailrelationshipwithonetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterToDetailRelationshipWithOneTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
