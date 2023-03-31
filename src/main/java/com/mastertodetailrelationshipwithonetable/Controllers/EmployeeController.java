b2072f112ea06afce27389a921eb6c848bf7c024
Ghulam Asghar Dahri <62570535+AsgharDahri@users.noreply.github.com>
2023-04-01 02:37:58 +0500

    Revert "Completed! (#1)"
    This reverts commit 4885abee522221a4cb6ee67446d8ad8e3591a363.

--- src/main/java/com/mastertodetailrelationshipwithonetable/Controllers/EmployeeController.java
+++ src/main/java/com/mastertodetailrelationshipwithonetable/Controllers/EmployeeController.java
@@ -1,14 +1,7 @@
 package com.mastertodetailrelationshipwithonetable.Controllers;
 
-
-import com.mastertodetailrelationshipwithonetable.Models.EmployeeModel;
-import com.mastertodetailrelationshipwithonetable.Models.EmpoloyeeDTO;
-import com.mastertodetailrelationshipwithonetable.Repository.Repo;
-import org.json.JSONObject;
-
 import com.mastertodetailrelationshipwithonetable.Models.Employee;
 import com.mastertodetailrelationshipwithonetable.Repository.Repo;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +11,8 @@ public class EmployeeController {
     @Autowired
     private Repo repo;
     @PostMapping("/save")
-
-    public EmployeeModel save(@RequestBody EmpoloyeeDTO employeeDto){
-        EmployeeModel employeeModel =new EmployeeModel();
-        employeeModel.setEmpName(employeeDto.getEmpName());
-        employeeModel.setEmpDesignation(employeeDto.getEmpDesignation());
-        employeeModel.setEmployeeDetailTable(new JSONObject(employeeDto.getEmployeeDetailDto()).toString());
-        return repo.save(employeeModel);
-
     public Employee save(@RequestBody Employee employee){
         return repo.save(employee);
-
     }
 
 
--- src/main/java/com/mastertodetailrelationshipwithonetable/Repository/Repo.java
+++ src/main/java/com/mastertodetailrelationshipwithonetable/Repository/Repo.java
@@ -1,14 +1,7 @@
 package com.mastertodetailrelationshipwithonetable.Repository;
 
-
-import com.mastertodetailrelationshipwithonetable.Models.EmployeeModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface Repo extends CrudRepository<EmployeeModel,String> {
-
 import com.mastertodetailrelationshipwithonetable.Models.Employee;
 import org.springframework.data.repository.CrudRepository;
 
 public interface Repo extends CrudRepository<Employee,String> {
-
 }
